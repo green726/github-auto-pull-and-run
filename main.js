e150475eee391ec7ff94e040a0819937559ce36e
green726 <59667714+green726@users.noreply.github.com>
2021-10-05 19:48:44 -0400

    made better

--- main.js
+++ main.js
@@ -1,26 +1,70 @@
-var fs = require('fs')
-var express = require("express");
-var app = express();
-var http = require("http").createServer(app);
-var clone = require('git-clone');
-var spawn = require('child_process').spawn;
+let fs = require('fs')
+//express stuff
+let express = require("express");
+let app = express();
+let http = require("http").createServer(app);
+//git clone package
+let clone = require('git-clone');
+//child process things
+let spawn = require('child_process').spawn;
+const { SIGINT } = require('constants');
+//port to listen for github API
+let port = 9090;
+//config file
+let config;
+//different child processes
+let child;
+let child2;
+let child3;
+//has child1 existed, if so need to kill the server before starting again
+let childHasExisted = false;
+
+
 
 fs.rmdirSync("./repo", { recursive: true });
 clone("https://github.com/AstrocoreGames/astrocoreBeta", "./repo")
-setTimeout(startServer, 5000);
-
+setTimeout(init, 200)
+setTimeout(pull, 2000)
+setTimeout(killChild, 3000);
+//TODO: write code to copy the repo data.dat to dataPersist.dat before pulling new repo and after pull new repo replace new data.dat with persistentData.dat
 app.post("/github", (req, res) => {
-    fs.rmdirSync("./repo", { recursive: true });
-    clone("https://github.com/AstrocoreGames/astrocoreBeta", "./repo")
-    setTimeout(startServer, 5000)
+    pull()
+    setTimeout(killChild, 600)
     console.log('Done')
     res.send("Done")
 })
 
+function killChild(){
+  if (childHasExisted){
+    child.kill("SIGINT")
+    console.log("killed child")
+    child = spawn('npx kill-port 8000', {
+      shell: true
+    })
+    child.stderr.on('data', function (data) {
+      console.error("Server error:", data.toString());
+    });
+  child.stdout.on('data', function (data) {
+      console.log("Server log:", data.toString());
+    });
+  child.on('exit', function (exitCode) {
+      console.log("Child exited with code: " + exitCode);
+    });
+    setTimeout(function(){ child.kill("SIGINT")}, 1000)
+
+    setTimeout(startServer, 5000)
+  } else{
+    setTimeout(startServer, 5000)
+  }
+  
+}
+
 function startServer() {
-    var child = spawn('cd ./repo && echo 1 && npm i && echo 2 && node server.js && echo 3', {
+      child = spawn('cd ./repo && echo 1 && npm i && echo 2 && node server.js && echo 3', {
         shell: true
       });
+    
+    childHasExisted = true;
     child.stderr.on('data', function (data) {
         console.error("Server error:", data.toString());
       });
@@ -32,8 +76,38 @@ function startServer() {
       });
 }  
 
+function pull(){
+  child2 = spawn('cd ./repo && git pull https://github.com/AstrocoreGames/astrocoreBeta', {
+    shell: true
+  })
+  child2.stderr.on('data', function (data) {
+    console.error("Git pull error:", data.toString());
+  });
 
-http.listen(9090, "0.0.0.0", () => {
+
+  setTimeout(() => {
+    child2.kill("SIGINT")
+    console.log("killed child 2")
+  }, 2000)
+}
+
+function init(){
+  child3 = spawn('cd ./repo && git init', {
+    shell: true
+  })
+  child3.stderr.on('data', function (data) {
+    console.error("Git init error:", data.toString());
+  });
+
+
+  setTimeout(() => {
+    child3.kill("SIGINT")
+    console.log("killed child 3")
+  }, 2000)
+}
+
+
+http.listen(port, "0.0.0.0", () => {
     console.log("listening on *:9090");
   });
 
